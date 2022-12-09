befec2077927fae050e50fd61d7538d05c961c46
FlorentinTh <florentin.thullier1@uqac.ca>
2022-12-09 10:43:55 -0500

    feat(index): add an index as first page to allow loading application external components

---
+++ src/scripts/index.js
@@ -0,0 +1,14 @@
+import '../styles/main.css';
+
+import { LoaderOverlay } from './components/loader-overlay.js';
+import { Router } from './utils/router.js';
+
+const loaderOverlay = new LoaderOverlay();
+loaderOverlay.toggle('Loading Application Components...');
+
+const router = new Router();
+router.disableBackButton();
+
+setTimeout(() => {
+  router.switchPage('data-discovering.html');
+}, 2000);
