f6f957d2c52af929473019efb2c886e30511c1eb
FlorentinTh <florentin.thullier1@uqac.ca>
2023-04-03 08:53:55 -0400

    fix(string-helper): more permissive on the labels of participant folders

--- src/scripts/helpers/string-helper.js
+++ src/scripts/helpers/string-helper.js
@@ -13,7 +13,15 @@ export class StringHelper {
   static revertParticipantNameFromSession(participant) {
     const participantArray = participant.split('_');
     const participantID = participantArray.slice(1, participantArray.length - 1);
-    const participantSex = participantArray[participantArray.length - 1];
-    return `${participantID} (${participantSex.toUpperCase()})`;
+
+    let participantIndicator;
+
+    if (participantArray.length > 2) {
+      participantIndicator = participantArray[participantArray.length - 1];
+    }
+
+    return participantIndicator === undefined
+      ? `${participantID}`
+      : `${participantID} (${participantIndicator.toUpperCase()})`;
   }
 }
