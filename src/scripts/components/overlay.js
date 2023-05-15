a8bfbefaff8a65867577e440915d75a112c39c62
FlorentinTh <florentin.thullier1@uqac.ca>
2023-05-15 08:46:04 -0400

    feat(overlay): add redirect parameter

--- src/scripts/components/overlay.js
+++ src/scripts/components/overlay.js
@@ -4,6 +4,7 @@ import successTemplate from '../../views/partials/components/success.hbs';
 import errorTemplate from '../../views/partials/components/error.hbs';
 
 import { TypeHelper } from '../helpers/type-helper.js';
+import { Router } from '../routes/router';
 
 const OverlayType = {
   ERROR: 'error',
@@ -15,19 +16,25 @@ class Overlay {
   #details;
   #interact;
   #type;
+  #redirect;
 
-  constructor(type, opts = { message: null, details: null, interact: false }) {
-    const defaultOpts = { message: null, details: null, interact: false };
+  constructor(
+    type,
+    opts = { message: null, details: null, interact: false, redirect: null }
+  ) {
+    const defaultOpts = { message: null, details: null, interact: false, redirect: '' };
     opts = { ...defaultOpts, ...opts };
 
     TypeHelper.checkStringNotNull(opts.message, { label: 'message' });
     TypeHelper.checkStringNotNull(opts.details, { label: 'details' });
     TypeHelper.checkBoolean(opts.interact, { label: 'interact' });
+    TypeHelper.checkString(opts.redirect, { label: 'redirect' });
 
     this.#message = opts.message;
     this.#details = opts.details;
     this.#interact = opts.interact;
     this.#type = type;
+    this.#redirect = opts.redirect;
   }
 
   show() {
@@ -66,21 +73,27 @@ class Overlay {
           overlay = document.getElementById('success-overlay');
         }
 
-        overlay.remove();
-        body.classList.remove('hide-overflow');
+        if (this.#redirect === '') {
+          overlay.remove();
+          body.classList.remove('hide-overflow');
+        } else {
+          const router = new Router();
+          router.disableBackButton();
+          router.switchPage(this.#redirect);
+        }
       });
     }
   }
 }
 
 export class ErrorOverlay extends Overlay {
-  constructor(opts = { message: null, details: null, interact: false }) {
+  constructor(opts = { message: null, details: null, interact: false, redirect: '' }) {
     super(OverlayType.ERROR, opts);
   }
 }
 
 export class SuccessOverlay extends Overlay {
-  constructor(opts = { message: null, details: null, interact: false }) {
+  constructor(opts = { message: null, details: null, interact: false, redirect: '' }) {
     super(OverlayType.SUCCESS, opts);
   }
 }
