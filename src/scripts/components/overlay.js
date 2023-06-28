f905336e3d7163bcd59ac4d2b370459a42d2e68e
FlorentinTh <florentin.thullier1@uqac.ca>
2023-06-28 09:03:37 -0400

    feat(overlay): add ability to customize the label of the interact button

--- src/scripts/components/overlay.js
+++ src/scripts/components/overlay.js
@@ -17,24 +17,39 @@ class Overlay {
   #interact;
   #type;
   #redirect;
+  #interactBtnLabel;
 
   constructor(
     type,
-    opts = { message: null, details: null, interact: false, redirect: null }
+    opts = {
+      message: null,
+      details: null,
+      interact: false,
+      redirect: null,
+      interactBtnLabel: null
+    }
   ) {
-    const defaultOpts = { message: null, details: null, interact: false, redirect: '' };
+    const defaultOpts = {
+      message: null,
+      details: null,
+      interact: false,
+      redirect: '',
+      interactBtnLabel: ''
+    };
     opts = { ...defaultOpts, ...opts };
 
     TypeHelper.checkStringNotNull(opts.message, { label: 'message' });
     TypeHelper.checkStringNotNull(opts.details, { label: 'details' });
     TypeHelper.checkBoolean(opts.interact, { label: 'interact' });
     TypeHelper.checkString(opts.redirect, { label: 'redirect' });
+    TypeHelper.checkString(opts.interactBtnLabel, { label: 'interactBtnLabel' });
 
     this.#message = opts.message;
     this.#details = opts.details;
     this.#interact = opts.interact;
     this.#type = type;
     this.#redirect = opts.redirect;
+    this.#interactBtnLabel = opts.interactBtnLabel;
   }
 
   show() {
@@ -42,16 +57,26 @@ class Overlay {
     let template;
 
     if (this.#type === OverlayType.ERROR) {
+      if (this.#interactBtnLabel === '') {
+        this.#interactBtnLabel = 'I Understand!';
+      }
+
       template = errorTemplate({
         message: this.#message,
         details: this.#details,
-        interact: this.#interact
+        interact: this.#interact,
+        interactBtnLabel: this.#interactBtnLabel
       });
     } else if (this.#type === OverlayType.SUCCESS) {
+      if (this.#interactBtnLabel === '') {
+        this.#interactBtnLabel = 'OK';
+      }
+
       template = successTemplate({
         message: this.#message,
         details: this.#details,
-        interact: this.#interact
+        interact: this.#interact,
+        interactBtnLabel: this.#interactBtnLabel
       });
     }
 
