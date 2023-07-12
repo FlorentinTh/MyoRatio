2816f85a2e6acd92d8ec50ef24d8990107fe42f3
FlorentinTh <florentin.thullier1@uqac.ca>
2023-07-12 12:20:56 -0400

    build(project): update build tasks to automate the copy of the API release and secret key

---
+++ api-copy.js
@@ -0,0 +1,39 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const recursiveCopy = (source, target) => {
+  if (fs.lstatSync(source).isDirectory()) {
+    if (!fs.existsSync(target)) {
+      fs.mkdirSync(target);
+    }
+
+    const files = fs.readdirSync(source);
+
+    files.forEach(file => {
+      const sourcePath = path.join(source, file);
+      const targetPath = path.join(target, file);
+      recursiveCopy(sourcePath, targetPath);
+    });
+  } else {
+    fs.copyFileSync(source, target);
+  }
+};
+
+const envBuildFile = fs.readFileSync(path.normalize('./env.build.json'));
+const envBuildData = JSON.parse(envBuildFile);
+const APIPath = path.normalize(envBuildData.API_PATH);
+
+const APISourceFolder = path.join(APIPath, 'dist', 'MyoRatioAPI');
+const targetFolder = 'bin/MyoRatioAPI';
+
+if (fs.existsSync(targetFolder)) {
+  fs.remove(targetFolder, error => {
+    if (error) {
+      throw new Error(error);
+    }
+
+    recursiveCopy(APISourceFolder, targetFolder);
+  });
+} else {
+  recursiveCopy(APISourceFolder, targetFolder);
+}
---
+++ api-secret.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const fs = require('fs');
+
+const envBuildFile = fs.readFileSync(path.normalize('./env.build.json'));
+const envBuildData = JSON.parse(envBuildFile);
+
+const APIPath = path.normalize(envBuildData.API_PATH);
+const APIEnvFile = fs.readFileSync(path.join(APIPath, '.env'));
+const APIEnvData = APIEnvFile.toString();
+
+const lines = APIEnvData.split(/\r\n|\r|\n/g);
+
+let APIKeyLine = null;
+for (const line of lines) {
+  if (line.includes('API_KEY = ')) {
+    APIKeyLine = line;
+    break;
+  }
+}
+
+let key = null;
+if (!(APIKeyLine === null)) {
+  const regexp = /(?<=")[^"]*(?=")/;
+  key = APIKeyLine.match(regexp)[0];
+}
+
+const envAppFilePath = path.normalize('./env.app.json');
+const envAppFile = fs.readFileSync(envAppFilePath);
+let envAppData = JSON.parse(envAppFile);
+
+envAppData.API_KEY = key;
+envAppData = JSON.stringify(envAppData, null, 2);
+
+fs.writeFileSync(envAppFilePath, envAppData, 'utf8');
